5fca8540e572ab81977f1c380c638df142a85db7
mehwish <mehwishgul715@gmail.com>
2022-07-08 15:09:50 +0500

    commit 3

--- assets/app.js
+++ assets/app.js
@@ -15,24 +15,24 @@ function openCity(evt, cityName) {
 
     // Gets the video src from the data-src on each button
     
-    var $videoSrc;  
-    $('.video-btn').click(function() {
-        $videoSrc = $(this).data( "src" );
+    // var $videoSrc;  
+    // $('.video-btn').click(function() {
+    //     $videoSrc = $(this).data( "src" );
       
-    });
+    // });
 
-    console.log($videoSrc);
+    // console.log($videoSrc);
       
-    $('#myModal').on('shown.bs.modal', function (e) {
+    // $('#myModal').on('shown.bs.modal', function (e) {
         
-    $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
-    })
+    // $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+    // })
       
     
     
-    $('#myModal').on('hide.bs.modal', function (e) {
-        $("#video").attr('src',$videoSrc); 
-    }) 
+    // $('#myModal').on('hide.bs.modal', function (e) {
+    //     $("#video").attr('src',$videoSrc); 
+    // }) 
         
         
 
